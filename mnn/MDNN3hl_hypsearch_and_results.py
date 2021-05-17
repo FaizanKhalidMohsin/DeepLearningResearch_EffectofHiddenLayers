f8f34bb643cb7d2fc2e96f2efca935b3dd117545
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2021-05-17 11:08:12 -0400

    Seventh Commit

--- mnn/MDNN3hl_hypsearch_and_results.py
+++ mnn/MDNN3hl_hypsearch_and_results.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tqdm import tqdm
 # We load the data
 
-msnn = pd.read_csv("Experimental_results/Experiment5_MultivariableShallowNN_HyperparamSearchResults.csv")
+msnn = pd.read_csv("../Data/Experimental_results/Experiment5_MultivariableShallowNN_HyperparamSearchResults.csv")
 print(msnn.shape)
 print(msnn.head())
 
